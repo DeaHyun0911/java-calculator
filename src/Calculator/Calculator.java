0e9afe809d5e6c3768a7ff7e2f7ed27c0519d96f
최대현 <eogus0911@gmail.com>
2024-11-12 10:39:44 +0900

    project setting

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,11 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print();
+    }
+}
