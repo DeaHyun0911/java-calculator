f3887d9d32a5f565feb2fe2ce38e399d31703a33
최대현 <eogus0911@gmail.com>
2024-11-12 16:36:39 +0900

    init: basic setting

---
+++ src/classCalculator/App.java
@@ -0,0 +1,4 @@
+package classCalculator;
+
+public class App {
+}
---
+++ src/classCalculator/Calculator.java
@@ -0,0 +1,18 @@
+package classCalculator;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Calculator {
+
+    int num1;
+    int num2;
+    char plus = '+';
+    char minus = '-';
+    char multi = '*';
+    char div = '/';
+
+    Queue<Integer> resultList = new LinkedList<Integer>();
+
+
+}
