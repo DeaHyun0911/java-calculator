53547923f3fe362e1d430ffbe8268ab95b5323f4
최대현 <eogus0911@gmail.com>
2024-11-12 14:15:06 +0900

    :sparkles: basic calculator

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -6,6 +6,32 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print();
+        System.out.print("첫번째 숫자: ");
+        int num1 = input.nextInt();
+
+        System.out.print("두번째 숫자: ");
+        int num2 = input.nextInt();
+
+        System.out.println("뭘로 계산할꺼야(+,-,*,/): ");
+        char operator = input.next().charAt(0);
+
+        double result = 0;
+
+        if(operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            result = num1 / num2;
+        } else {
+            System.out.println("잘못 입력하였습니다.");
+        }
+
+        System.out.print(num1 + " " + operator + " " + num2 + " = " + result);
+
+
     }
+
 }
