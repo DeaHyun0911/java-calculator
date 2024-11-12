4c069859e58283fa3fa9339d30bac8926818e260
최대현 <eogus0911@gmail.com>
2024-11-12 20:37:59 +0900

    feat: menu add

--- src/classCalculator/App.java
+++ src/classCalculator/App.java
@@ -6,30 +6,50 @@ public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Calculator calculator = new Calculator();
+        char operator;
 
         while(true) {
-            System.out.print("첫번째 숫자 입력: ");
-            int num1 = input.nextInt();
-            System.out.print("두번째 숫자 입력: ");
-            int num2 = input.nextInt();
-            System.out.print("연산기호: ");
-            char operator = input.next().charAt(0);
 
-            calculator.calculate(num1, num2, operator);
+                // 첫번째 계산
+                if(calculator.getListEmpty()) {
+                    System.out.print("첫번째 숫자 입력: ");
+                    int num1 = input.nextInt();
+                    System.out.print("두번째 숫자 입력: ");
+                    int num2 = input.nextInt();
+                    System.out.print("연산기호: ");
+                    operator = input.next().charAt(0);
 
-            calculator.getResult();
+                    calculator.calculate(num1, num2, operator);
+                    calculator.getResult();
+                } else {
 
-            // 재시작 체크
-            System.out.print("다시 계산할꺼야? (exit 입력시 종료 / 아무거나 입력시 재시작): ");
-            input.nextLine();
-            String str = input.nextLine();
-            if (str.equals("exit")) {
-                break;
+                // 재시작 체크
+                System.out.println("[ 메뉴를 선택하세요 (번호 입력) ]");
+                System.out.println("1. 계속 계산 2. 리셋하기 3. 리스트 조회 4. 종료하기");
+                System.out.print("메뉴 선택: ");
+                int menu = input.nextInt();
+
+                if (menu == 1) {
+                    System.out.print("숫자 입력: ");
+                    int num = input.nextInt();
+
+                    System.out.print("연산기호: ");
+                    operator = input.next().charAt(0);
+
+                    calculator.calculate(calculator.getCurrentNumber(), num, operator);
+                    calculator.getResult();
+                } else if (menu == 2) {
+                    calculator.setResetList();
+                } else if (menu == 3) {
+                    String list = calculator.getList();
+                    System.out.println(list);
+                } else if (menu == 4) {
+                    break;
+                }
             }
+
+
+
         }
-
-
-
-
     }
 }
--- src/classCalculator/Calculator.java
+++ src/classCalculator/Calculator.java
@@ -5,33 +5,50 @@ import java.util.Stack;
 public class Calculator {
 
     // 결과 저장 콜렉션
-    private Stack<Integer> resultList = new Stack<Integer>();
+    private Stack<Integer> resultStack = new Stack<Integer>();
 
     // 연산 메서드
     public Stack<Integer> calculate(int num1, int num2, char operator) {
         switch (operator) {
             case '+':
-                resultList.add(num1 + num2);
+                resultStack.add(num1 + num2);
                 break;
             case '-':
-                resultList.add(num1 - num2);
+                resultStack.add(num1 - num2);
                 break;
             case '*':
-                resultList.add(num1 * num2);
+                resultStack.add(num1 * num2);
                 break;
             case '/':
-                resultList.add(num1 / num2);
+                resultStack.add(num1 / num2);
                 break;
         }
-        return resultList;
+        return resultStack;
     }
 
     // 결과 출력 메서드
     public void getResult() {
-        System.out.println(resultList.peek());
+        System.out.println(resultStack.peek());
     }
 
+    // 최근 값 내보내기
+    public int getCurrentNumber() {
+        return resultStack.peek();
+    }
 
+    // 리스트 리셋
+    public void setResetList() {
+        resultStack.clear();
+        System.out.println("리셋되었습니다.");
+    }
 
+    // 리스트가 비었는지 확인
+    public boolean getListEmpty() {
+        return resultStack.isEmpty();
+    }
 
+    // 리스트 조회
+    public String getList() {
+        return resultStack.toString();
+    }
 }
